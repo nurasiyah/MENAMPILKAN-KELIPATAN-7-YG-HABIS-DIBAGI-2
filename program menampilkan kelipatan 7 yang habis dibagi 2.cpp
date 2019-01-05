22a929550ce68f5eae4bc5075b5de488c6339c49
nurasiyah <45659380+nurasiyah@users.noreply.github.com>
2019-01-05 15:10:52 +0700

    Add files via upload

---
+++ program menampilkan kelipatan 7 yang habis dibagi 2.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+   int main();
+    printf ("KELIPATAN & YANG HABIS DIBAGI 2(1=50).\n\n");
+    int i=7;
+    while(i<50)
+    {
+        if (i%2==0&&i%7==0)
+        {
+            printf("\n%d",i);
+        } i++;
+    }
+    getch();
+}
